02b0d6bc6801de173a7e43f56f591c6d79aa98fc
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-01-19 01:13:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
+++ Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
@@ -1,15 +1,15 @@
-class Solution {
+class Solution{
 public:
-long long minCost(vector<int>& a) {
-priority_queue<long long, vector<long long>, greater<long long>> pq;
-for (int x : a) pq.push(x);
-long long cost = 0;
-while (pq.size() > 1) {
-long long x = pq.top(); pq.pop();
-long long y = pq.top(); pq.pop();
-cost += x + y;
-pq.push(x + y);
-}
-return cost;
-}
+    long long minCost(vector<int>& a){
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        for(int x : a) pq.push(x);
+        long long cost = 0;
+        while(pq.size() > 1){
+            long long x = pq.top(); pq.pop();
+            long long y = pq.top(); pq.pop();
+            cost += x + y;
+            pq.push(x + y);
+        }
+        return cost;
+    }
 };
