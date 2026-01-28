3a9386b604cee897951bfa82a484ed99b728580f
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-01-28 12:55:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse digits/reverse-digits.cpp
+++ Difficulty: Basic/Reverse digits/reverse-digits.cpp
@@ -1,11 +1,16 @@
-class Solution {
-  public:
-    int reverseDigits(int n) {
-        int r=0;
-        while(n){
-            r=r*10+n%10;
-            n/=10;
-        }
-        return r;
-    }
+class Solution
+{
+ public:
+ int reverseDigits(int n)
+ {
+     long long r = 0;
+     while(n)
+     {
+     r = r*10+n%10;
+     n/=10;
+     }
+     return r;    
+ }
+ 
+ 
 };
