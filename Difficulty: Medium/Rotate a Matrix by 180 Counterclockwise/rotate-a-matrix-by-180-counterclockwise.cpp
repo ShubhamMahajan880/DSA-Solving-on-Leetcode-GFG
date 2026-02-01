77c4726035a92ab4744dfae42b60cbafa9d47577
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-02-01 23:58:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate a Matrix by 180 Counterclockwise/rotate-a-matrix-by-180-counterclockwise.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    void rotateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        
+        for(int i=0; i<n/2; i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                swap(mat[i][j], mat[n-i-1][j]);
+            }
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            reverse(mat[i].begin(),mat[i].end());    
+        }
+        
+        // return mat;
+    }
+};
