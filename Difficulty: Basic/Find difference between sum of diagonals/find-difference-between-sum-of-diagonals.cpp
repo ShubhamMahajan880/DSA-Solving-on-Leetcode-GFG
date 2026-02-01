e520dc7a4762547bdb911975220c9c0ab2c0f18c
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-02-01 22:08:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find difference between sum of diagonals/find-difference-between-sum-of-diagonals.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int diagonalSumDifference(int N, vector<vector<int>> Grid) {
+        int sum1 = 0;
+        int sum2 = 0;
+        
+        for(int i = 0; i < N; i++) {
+            sum1 += Grid[i][i];
+            sum2 += Grid[i][N - i - 1];
+        }
+        
+        return abs(sum1 - sum2);
+    }
+};
