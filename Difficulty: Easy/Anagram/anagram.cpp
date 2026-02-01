ac3c74e15f276344a1f73c828b8dcf92b55bec99
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-02-01 15:28:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    bool areAnagrams(string& s1, string& s2) {
+        if(s1.length() != s2.length())
+        {
+            return false;
+        }
+        
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        
+        if(s1 == s2)
+        {
+            return true;
+        }
+        else
+        return false;
+        
+    }
+};
