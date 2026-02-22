e13d40fb4a408f50fe9a3cee93eb2626f4ad93f8
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-02-23 00:22:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth largest element/kth-largest-element.cpp
@@ -0,0 +1,11 @@
+class Solution{
+public:
+    int KthLargest(vector<int>& arr, int k){
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int x: arr){
+            pq.push(x);
+            if(pq.size()>k) pq.pop();
+        }
+        return pq.top();
+    }
+};
