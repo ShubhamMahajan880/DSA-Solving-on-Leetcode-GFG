3c1366e66b00f1909d24d301505c326e473e595f
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-02-26 14:16:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
+++ Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     
-    void dfs(int node,
-             vector<vector<int>>& adj,
-             vector<int>& visited,
-             vector<int>& component){
-        
-        visited[node] = 1;
-        component.push_back(node);
-        
-        for(int nei : adj[node]){
-            if(!visited[nei]){
-                dfs(nei, adj, visited, component);
-            }
-        }
-    }
-    
     vector<vector<int>> getComponents(int V,
                                       vector<vector<int>>& edges) {
         
@@ -31,8 +16,27 @@ public:
         
         for(int i = 0; i < V; i++){
             if(!visited[i]){
+                
                 vector<int> component;
-                dfs(i, adj, visited, component);
+                queue<int> q;
+                
+                q.push(i);
+                visited[i] = 1;
+                
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    
+                    component.push_back(node);
+                    
+                    for(int nei : adj[node]){
+                        if(!visited[nei]){
+                            visited[nei] = 1;
+                            q.push(nei);
+                        }
+                    }
+                }
+                
                 result.push_back(component);
             }
         }
