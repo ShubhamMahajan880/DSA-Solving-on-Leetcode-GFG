199c6e151c6518d993242916992db235b6b5114f
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-03-27 08:15:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int knapsack(int capacity, vector<int>& val, vector<int>& wt) {
-        
         int n = val.size();
+        
         vector<vector<int>> dp(n+1, vector<int>(capacity+1, 0));
         
-        for(int i = 1; i <= n; i++) {
-            for(int w = 0; w <= capacity; w++) {
-                
-                if(wt[i-1] <= w)
-                    dp[i][w] = max(
-                        val[i-1] + dp[i-1][w - wt[i-1]],
-                        dp[i-1][w]
-                    );
-                else
+        for(int i = 1; i <= n; i++){
+            for(int w = 1; w <= capacity; w++){
+                if(wt[i-1] <= w){
+                    dp[i][w] = max(val[i-1] + dp[i-1][w - wt[i-1]], dp[i-1][w]);
+                } else {
                     dp[i][w] = dp[i-1][w];
+                }
             }
         }
         
