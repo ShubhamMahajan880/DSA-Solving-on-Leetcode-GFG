e6cc0686178a6c350f4b4e17fd192d2bc822616e
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-04-18 11:54:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Serialize and deserialize a binary tree/serialize-and-deserialize-a-binary-tree.cpp
@@ -0,0 +1,52 @@
+class Solution{
+public:
+    vector<int> serialize(Node *root) {
+        vector<int> res;
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            Node* node = q.front();
+            q.pop();
+            
+            if(node){
+                res.push_back(node->data);
+                q.push(node->left);
+                q.push(node->right);
+            } else {
+                res.push_back(-1);
+            }
+        }
+        
+        return res;
+    }
+
+    Node* deSerialize(vector<int> &arr) {
+        if(arr.size() == 0) return NULL;
+        
+        Node* root = new Node(arr[0]);
+        queue<Node*> q;
+        q.push(root);
+        
+        int i = 1;
+        
+        while(!q.empty() && i < arr.size()){
+            Node* curr = q.front();
+            q.pop();
+            
+            if(arr[i] != -1){
+                curr->left = new Node(arr[i]);
+                q.push(curr->left);
+            }
+            i++;
+            
+            if(i < arr.size() && arr[i] != -1){
+                curr->right = new Node(arr[i]);
+                q.push(curr->right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+};
