80f3a5f812eeaba9d7a4c97dce8db0de6ca437d9
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-04-22 11:31:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Diagonal Tree Traversal/diagonal-tree-traversal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<int> diagonal(Node *root) {
+        vector<int> res;
+        if(!root) return res;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            Node* node = q.front();
+            q.pop();
+            
+            while(node){
+                res.push_back(node->data);
+                
+                if(node->left) q.push(node->left);
+                
+                node = node->right;
+            }
+        }
+        
+        return res;
+    }
+};
