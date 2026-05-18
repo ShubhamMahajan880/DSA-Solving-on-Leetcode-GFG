1371f0e7d4a4717bdc42545eee2198f3c8004c7e
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-05-18 11:20:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Populate Inorder Successors/populate-inorder-successors.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+    Node* next = NULL;
+    
+    void populateNext(Node *root){
+        if(!root) return;
+        
+        populateNext(root->right);
+        
+        root->next = next;
+        next = root;
+        
+        populateNext(root->left);
+    }
+};
