457dcc65d9a3b02625c3cb3b917ec49b6b3f144d
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-05-26 11:12:19 +0530

    Time: 11 ms (17.07%) | Memory: 38.8 MB (29.71%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> s;
+        return dfs(root,k,s);
+    }
+    
+    bool dfs(TreeNode* root,int k,unordered_set<int>& s){
+        if(!root) return false;
+        
+        if(s.count(k-root->val)) return true;
+        
+        s.insert(root->val);
+        
+        return dfs(root->left,k,s) ||
+               dfs(root->right,k,s);
+    }
+};
