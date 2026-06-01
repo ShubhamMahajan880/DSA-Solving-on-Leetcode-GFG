d2f366359e3960e763ae429cd11e7a7c970d4827
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-06-02 00:33:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Balanced Tree Check/balanced-tree-check.cpp
+++ Difficulty: Easy/Balanced Tree Check/balanced-tree-check.cpp
@@ -1,20 +1,20 @@
 class Solution {
-  public:
-    bool isBalanced(Node *root) {
-        return check(root) != -1;
-    }
-    
-    int check(Node* node){
-        if(!node) return 0;
-        
-        int lh = check(node->left);
+public:
+    int solve(Node* root){
+        if(!root) return 0;
+
+        int lh = solve(root->left);
         if(lh == -1) return -1;
-        
-        int rh = check(node->right);
+
+        int rh = solve(root->right);
         if(rh == -1) return -1;
-        
+
         if(abs(lh - rh) > 1) return -1;
-        
+
         return 1 + max(lh, rh);
+    }
+
+    bool isBalanced(Node* root) {
+        return solve(root) != -1;
     }
 };
