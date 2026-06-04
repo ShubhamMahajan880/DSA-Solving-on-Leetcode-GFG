bb1f59b1ac6870b56ee436646f2837565d9e422d
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-06-05 00:57:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
+++ Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    bool findPath(Node* root, int x, vector<Node*>& path) {
-        if (root == NULL) return false;
-        path.push_back(root);
-        if (root->data == x) return true;
-        if (findPath(root->left, x, path) || findPath(root->right, x, path))
-            return true;
-        path.pop_back();
-        return false;
-    }
-
     Node* lca(Node* root, int n1, int n2) {
-        vector<Node*> p1, p2;
-        if (!findPath(root, n1, p1) || !findPath(root, n2, p2))
-            return NULL;
-        int i = 0;
-        while (i < p1.size() && i < p2.size() && p1[i] == p2[i]) i++;
-        return p1[i - 1];
+        if(!root) return NULL;
+
+        if(root->data == n1 || root->data == n2)
+            return root;
+
+        Node* left = lca(root->left, n1, n2);
+        Node* right = lca(root->right, n1, n2);
+
+        if(left && right) return root;
+
+        return left ? left : right;
     }
 };
