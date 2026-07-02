dcbdbf04892d956671a21c2f5bc443c1c846dfd4
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-07-03 00:49:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor in BST/floor-in-bst.cpp
@@ -0,0 +1,20 @@
+class Solution {
+	public:
+	int findMaxFork(Node* root, int x) {
+		int ans = -1;
+		
+		while (root) {
+			if (root->data == x)
+				return x;
+			
+			if (root->data < x) {
+				ans = root->data;
+				root = root->right;
+			} else {
+				root = root->left;
+			}
+		}
+		
+		return ans;
+	}
+};
