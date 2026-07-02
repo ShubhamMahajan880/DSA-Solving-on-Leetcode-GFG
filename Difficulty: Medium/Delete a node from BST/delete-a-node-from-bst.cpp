095f4827b6a02bdb654e929c33f510758ee0693a
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-07-02 13:31:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Delete a node from BST/delete-a-node-from-bst.cpp
+++ Difficulty: Medium/Delete a node from BST/delete-a-node-from-bst.cpp
@@ -1,37 +1,39 @@
 class Solution {
-  public:
-    Node* delNode(Node* root, int key) {
-        if(!root) return NULL;
-        
-        if(key < root->data){
-            root->left = delNode(root->left, key);
-        }
-        else if(key > root->data){
-            root->right = delNode(root->right, key);
-        }
-        else{
-            if(!root->left){
-                Node* temp = root->right;
-                delete root;
-                return temp;
-            }
-            
-            if(!root->right){
-                Node* temp = root->left;
-                delete root;
-                return temp;
-            }
-            
-            Node* temp = root->right;
-            
-            while(temp->left){
-                temp = temp->left;
-            }
-            
-            root->data = temp->data;
-            root->right = delNode(root->right, temp->data);
-        }
-        
-        return root;
-    }
+	public:
+	Node* findMin(Node* root) {
+		while (root->left)
+			root = root->left;
+		return root;
+	}
+	
+	Node* delNode(Node* root, int key) {
+		if (!root)
+			return NULL;
+		
+		if (key < root->data) {
+			root->left = delNode(root->left, key);
+		}
+		else if (key > root->data) {
+			root->right = delNode(root->right, key);
+		}
+		else {
+			if (!root->left) {
+				Node* temp = root->right;
+				delete root;
+				return temp;
+			}
+			
+			if (!root->right) {
+				Node* temp = root->left;
+				delete root;
+				return temp;
+			}
+			
+			Node* temp = findMin(root->right);
+			root->data = temp->data;
+			root->right = delNode(root->right, temp->data);
+		}
+		
+		return root;
+	}
 };
