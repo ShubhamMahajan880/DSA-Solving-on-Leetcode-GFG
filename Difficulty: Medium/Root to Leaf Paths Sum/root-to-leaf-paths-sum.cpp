aa3cab6d662ea360683d5abbbc4a20f57c943931
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-07-15 14:45:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Root to Leaf Paths Sum/root-to-leaf-paths-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(Node* root, int num) {
+        if(!root)
+            return 0;
+
+        num = num * 10 + root->data;
+
+        if(!root->left && !root->right)
+            return num;
+
+        return solve(root->left, num) + solve(root->right, num);
+    }
+
+    int treePathsSum(Node *root) {
+        return solve(root, 0);
+    }
+};
