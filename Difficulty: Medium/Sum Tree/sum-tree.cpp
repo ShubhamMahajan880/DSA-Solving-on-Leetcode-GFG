86334aa40aba1cf1cc7b9c84b7a1cc4e1e991861
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-07-17 15:03:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum Tree/sum-tree.cpp
+++ Difficulty: Medium/Sum Tree/sum-tree.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    pair<bool,int> solve(Node* root){
-        if(root == NULL) return {true, 0};
-        
-        if(root->left == NULL && root->right == NULL)
+    pair<bool,int> solve(Node* root) {
+        if(!root)
+            return {true, 0};
+
+        if(!root->left && !root->right)
             return {true, root->data};
-        
+
         auto left = solve(root->left);
         auto right = solve(root->right);
-        
-        bool isValid = left.first && right.first && (root->data == left.second + right.second);
-        // 
-        return {isValid, left.second + right.second + root->data};
+
+        bool ok = left.first && right.first &&
+                  (root->data == left.second + right.second);
+
+        return {ok, left.second + right.second + root->data};
     }
-    
+
     bool isSumTree(Node* root) {
         return solve(root).first;
     }
