949f2d689687b4a29740709ec67505503f6215d7
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-08-04 11:26:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ancestors in Binary Tree/ancestors-in-binary-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool solve(Node* root, int target, vector<int>& ans) {
+        if(root == NULL)
+            return false;
+
+        if(root->data == target)
+            return true;
+
+        if(solve(root->left, target, ans) ||
+           solve(root->right, target, ans)) {
+            ans.push_back(root->data);
+            return true;
+        }
+
+        return false;
+    }
+
+    vector<int> ancestors(Node* root, int target) {
+        vector<int> ans;
+        solve(root, target, ans);
+        return ans;
+    }
+};
