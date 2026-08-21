d9e6e60cf0edfc498229ecda23809c7c4dbd32ce
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-08-22 00:55:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Maximum Width of Tree/maximum-width-of-tree.cpp
+++ Difficulty: Easy/Maximum Width of Tree/maximum-width-of-tree.cpp
@@ -1,25 +1,30 @@
 class Solution {
-  public:
+public:
     int maxWidth(Node* root) {
-        if(!root) return 0;
-        
+        if (root == nullptr)
+            return 0;
+
         queue<Node*> q;
         q.push(root);
+
         int ans = 0;
-        
-        while(!q.empty()){
-            int sz = q.size();
-            ans = max(ans, sz);
-            
-            while(sz--){
+
+        while (!q.empty()) {
+            int size = q.size();
+            ans = max(ans, size);
+
+            while (size--) {
                 Node* node = q.front();
                 q.pop();
-                
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
+
+                if (node->left)
+                    q.push(node->left);
+
+                if (node->right)
+                    q.push(node->right);
             }
         }
-        
+
         return ans;
     }
 };
