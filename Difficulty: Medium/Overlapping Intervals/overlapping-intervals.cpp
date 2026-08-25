86f2bfe10ab8b204f19de1d04941ad777aef3201
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-08-25 12:25:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        sort(arr.begin(), arr.end());
+
+        vector<vector<int>> ans;
+
+        for (auto interval : arr) {
+            if (ans.empty() || ans.back()[1] < interval[0]) {
+                ans.push_back(interval);
+            } else {
+                ans.back()[1] = max(ans.back()[1], interval[1]);
+            }
+        }
+
+        return ans;
+    }
+};
