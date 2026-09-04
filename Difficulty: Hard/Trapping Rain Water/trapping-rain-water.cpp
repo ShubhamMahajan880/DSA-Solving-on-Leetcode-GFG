b3a571e55627250b817a76be71e5ffc69f5f7d42
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-09-05 02:22:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxWater(vector<int>& arr) {
+        int left = 0;
+        int right = arr.size() - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+        int ans = 0;
+
+        while (left <= right) {
+            if (arr[left] <= arr[right]) {
+                if (arr[left] >= leftMax)
+                    leftMax = arr[left];
+                else
+                    ans += leftMax - arr[left];
+
+                left++;
+            } else {
+                if (arr[right] >= rightMax)
+                    rightMax = arr[right];
+                else
+                    ans += rightMax - arr[right];
+
+                right--;
+            }
+        }
+
+        return ans;
+    }
+};
