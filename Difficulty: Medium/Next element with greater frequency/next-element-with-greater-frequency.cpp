b6af8bce6744e22d734f8084cefa0a8a4801ceac
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2026-09-21 13:34:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextFreqGreater(vector<int>& arr) {
+        int n = arr.size();
+
+        unordered_map<int, int> freq;
+
+        for (int x : arr) {
+            freq[x]++;
+        }
+
+        vector<int> ans(n, -1);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && freq[arr[i]] > freq[arr[st.top()]]) {
+                ans[st.top()] = arr[i];
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
